a6a156934886916f8f1ead3d2700b90c8388c6af
vipxray <vipxray@hotmail.com>
2020-09-23 00:10:59 -0400

    Project establishment

---
+++ Coffee.java
@@ -0,0 +1,34 @@
+public class Coffee {
+
+    String coffeeName;
+    double coffeePrice;
+
+    public Coffee(String ip_coffeeName, double ip_coffeePrice) {
+        this.coffeeName = ip_coffeeName;
+        this.coffeePrice = ip_coffeePrice;
+    }
+
+
+    //    int coffeeID;
+    //    String coffeeName;
+    //    String shortDescription;
+    //    double coffeePrice;
+    //    double coffeeRating;
+    //    String flavor_note;
+    //    String flavor_category;
+    //    String roast_lv;
+//    public Coffee(int coffeeID, String coffeeName, String shortDescription,
+//        double coffeePrice, double coffeeRating,
+//        String flavor_note, String flavor_category, String roast_lv) {
+//    public Coffee(String coffeeName) {
+//                this.coffeeID = coffeeID;
+//                this.coffeeName = coffeeName;
+//                this.shortDescription = shortDescription;
+//                this.coffeePrice = coffeePrice;
+//                this.coffeeRating = coffeeRating;
+//                this.flavor_note = flavor_note;
+//                this.flavor_category = flavor_category;
+//                this.roast_lv = roast_lv;
+//            }
+
+}
